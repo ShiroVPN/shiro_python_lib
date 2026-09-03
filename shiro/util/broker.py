fed7c6f9e2921f71b6864694bc4b9a905cbb171d
Andrew <andrey.chikin1223@gmail.com>
2026-09-03 14:21:19 +0300

    added queue to client
    if task_queues = [], AioPikaBroker creates a default queue "taskiq" on
    startup.

--- shiro/util/broker.py
+++ shiro/util/broker.py
@@ -11,32 +11,38 @@ class BrokerConfigForClient(BaseModel):
     broker_url: AmqpDsn
     result_backend_url: RedisDsn
     exchange_name: str
+    queue_name: str
 
 
 class BrokerConfigForWorker(BrokerConfigForClient):
-    queue_name: str
     queue_bind_arguments: dict[str, str]
 
 
 def create_broker(
     config: BrokerConfigForClient | BrokerConfigForWorker,
 ) -> AioPikaBroker:
-    exchange = Exchange(name=config.exchange_name, type=ExchangeType.HEADERS)
-    task_queues = []
-    if isinstance(config, BrokerConfigForWorker):
-        task_queues = [
+    broker = (
+        AioPikaBroker(
+            url=str(config.broker_url),
+            exchange=Exchange(
+                name=config.exchange_name, type=ExchangeType.HEADERS
+            ),
+        )
+        .with_queue(
             Queue(
                 name=config.queue_name,
-                bind_arguments=cast(
-                    FieldTable, {"x-match": "all"} | config.queue_bind_arguments
+                bind_arguments=(
+                    cast(
+                        FieldTable,
+                        {"x-match": "all"} | config.queue_bind_arguments,
+                    )
+                    if isinstance(config, BrokerConfigForWorker)
+                    else {}
                 ),
             )
-        ]
-    broker = AioPikaBroker(
-        url=str(config.broker_url),
-        exchange=exchange,
-        task_queues=task_queues,
-    ).with_result_backend(
-        RedisAsyncResultBackend(str(config.result_backend_url))
+        )
+        .with_result_backend(
+            RedisAsyncResultBackend(str(config.result_backend_url))
+        )
     )
     return broker
