9b0e682cf75feb7eb1d019a7ea6dfa31ad2c7029
Andrew <andrey.chikin1223@gmail.com>
2026-09-03 14:56:07 +0300

    fixed client queue

--- shiro/util/broker.py
+++ shiro/util/broker.py
@@ -11,10 +11,10 @@ class BrokerConfigForClient(BaseModel):
     broker_url: AmqpDsn
     result_backend_url: RedisDsn
     exchange_name: str
-    queue_name: str
 
 
 class BrokerConfigForWorker(BrokerConfigForClient):
+    queue_name: str
     queue_bind_arguments: dict[str, str]
 
 
@@ -31,15 +31,13 @@ def create_broker(
         .with_queue(
             Queue(
                 name=config.queue_name,
-                bind_arguments=(
-                    cast(
-                        FieldTable,
-                        {"x-match": "all"} | config.queue_bind_arguments,
-                    )
-                    if isinstance(config, BrokerConfigForWorker)
-                    else {}
+                bind_arguments=cast(
+                    FieldTable,
+                    {"x-match": "all"} | config.queue_bind_arguments,
                 ),
             )
+            if isinstance(config, BrokerConfigForWorker)
+            else Queue(declare=False)
         )
         .with_result_backend(
             RedisAsyncResultBackend(str(config.result_backend_url))
